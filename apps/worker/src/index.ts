7fe7d5e9ab7dff25c1f5832367b559ddf09d11b1
Peter Mbanugo <p.mbanugo@yahoo.com>
2023-12-20 23:58:35 +0100

    add worker app

---
+++ apps/worker/src/index.ts
@@ -0,0 +1,38 @@
+import { NativeConnection, Worker } from "@temporalio/worker";
+import * as activities from "@cront-atlas/workflow/activities";
+import { constants } from "@cront-atlas/workflow";
+
+async function run() {
+  const address = process.env.TEMPORAL_SERVER_ADDRESS || "localhost:7233";
+  const certificate = process.env.TEMPORAL_TLS_CERTIFICATE;
+  const key = process.env.TEMPORAL_TLS_PRIVATE_KEY;
+
+  const connection = await NativeConnection.connect({
+    address,
+    tls:
+      certificate && key
+        ? {
+            clientCertPair: {
+              crt: Buffer.from(certificate),
+              key: Buffer.from(key),
+            },
+          }
+        : undefined,
+  });
+
+  const worker = await Worker.create({
+    workflowsPath: require.resolve("@cront-atlas/workflow/workflows"),
+    activities,
+    connection,
+    taskQueue: constants.QUEUE,
+    namespace: constants.NAMESPACE,
+  });
+
+  await worker.run();
+  connection.close();
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
